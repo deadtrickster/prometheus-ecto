43a337db95c6da95a81440936de4b44dac5519c0
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-21 02:39:58 +0300

    test nil times

--- test/prometheus_ecto_test.exs
+++ test/prometheus_ecto_test.exs
@@ -3,12 +3,16 @@ defmodule PrometheusEctoTest do
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Prometheus.EctoInstrumenter.TestRepo)
   end
+
+  alias Prometheus.EctoInstrumenter.TestSchema
+  alias Prometheus.EctoInstrumenter.TestRepo
+
   test "the truth" do
     assert 1 + 1 == 2
   end
 
   test "hello_world" do
-    result = Prometheus.EctoInstrumenter.TestRepo.query!("SELECT 1")
+    result = TestRepo.query!("SELECT 1")
     assert result.rows == [[1]]
     assert {buckets, sum} = :prometheus_histogram.value(:ecto_query_duration_microseconds, [:ok])
     assert sum > 0
@@ -25,7 +29,26 @@ defmodule PrometheusEctoTest do
     assert {buckets, sum} = :prometheus_histogram.value(:ecto_decode_duration_microseconds, [:ok])
     assert sum > 0
     assert 1 = Enum.reduce(buckets, fn(x, acc) -> x + acc end)
-  end
 
-  ### test transactioned insertion - there should be query with queue/decode_time=nil
+    changeset = TestSchema.changeset(%TestSchema{}, %{test_field: "qwe"})
+
+    ## transactioned insertion - there should be queries with queue/decode_time=nil
+    {:ok, _} = TestRepo.transaction(fn -> TestRepo.insert(changeset) end)
+
+    assert {buckets, sum} = :prometheus_histogram.value(:ecto_query_duration_microseconds, [:ok])
+    assert sum > 0
+    assert 4 = Enum.reduce(buckets, fn(x, acc) -> x + acc end)
+
+    assert {buckets, sum} = :prometheus_histogram.value(:ecto_queue_duration_microseconds, [:ok])
+    assert sum > 0
+    assert 2 = Enum.reduce(buckets, fn(x, acc) -> x + acc end)
+
+    assert {buckets, sum} = :prometheus_histogram.value(:ecto_db_query_duration_microseconds, [:ok])
+    assert sum > 0
+    assert 4 = Enum.reduce(buckets, fn(x, acc) -> x + acc end)
+
+    assert {buckets, sum} = :prometheus_histogram.value(:ecto_decode_duration_microseconds, [:ok])
+    assert sum > 0
+    assert 2 = Enum.reduce(buckets, fn(x, acc) -> x + acc end)
+  end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -15,8 +15,26 @@ end
 defmodule Prometheus.EctoInstrumenter.TestSchema do
   use Ecto.Schema
 
+  import Ecto.Changeset
+
   schema "test_schema" do
     field :test_field, :string
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, ~w(test_field))
+    |> validate_required([:test_field])
+  end
+end
+
+defmodule Prometheus.EctoInstrumenter.Migration do
+  use Ecto.Migration
+
+  def change do
+    create table(:test_schema) do
+      add :test_field, :text
+    end
   end
 end
 
@@ -24,4 +42,6 @@ Prometheus.EctoInstrumenter.setup()
 Application.ensure_all_started(:mariaex)
 Mix.Task.run "ecto.create", ~w(-r Prometheus.EctoInstrumenter.TestRepo)
 {:ok, _pid} = Prometheus.EctoInstrumenter.TestRepo.start_link
+{:ok, _pid} = Ecto.Migration.Supervisor.start_link
+Ecto.Migrator.up(Prometheus.EctoInstrumenter.TestRepo, 0, Prometheus.EctoInstrumenter.Migration, log: false)
 Ecto.Adapters.SQL.Sandbox.mode(Prometheus.EctoInstrumenter.TestRepo, :manual)
