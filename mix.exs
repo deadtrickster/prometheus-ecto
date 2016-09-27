f39b41a4ad1fdfb2c1900684c1536fa299becf1e
Ilya Khaprov <dead.trickster@gmail.com>
2016-09-27 11:12:03 +0300

    update prometheus.ex and bump to 1.0.0-rc1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PrometheusEcto.Mixfile do
   use Mix.Project
 
-  @version "1.0.0-alpha8"
+  @version "1.0.0-rc1"
 
   def project do
     [app: :prometheus_ecto,
@@ -39,7 +39,7 @@ defmodule PrometheusEcto.Mixfile do
   end
 
   defp deps do
-    [{:prometheus_ex, "~> 1.0.0-alpha8"},
+    [{:prometheus_ex, "~> 1.0.0-rc1"},
      {:ecto, "~> 2.0"},
      {:mariaex, ">= 0.0.0", only: :test},
      {:ex_doc, "~> 0.11", only: :dev},
